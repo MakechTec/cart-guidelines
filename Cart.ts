30d2c79ddb9977c4ffdb23a3760adabe60ef19da
Angel Dominguez <angeldomp49@gmail.com>
2022-06-11 23:04:30 -0500

    first commit

---
+++ Cart.ts
@@ -0,0 +1,13 @@
+import CartItem from "./CartItem";
+
+export default interface Cart{
+    
+    list( onSuccess: Function ) : void;
+    clean( onSuccess: Function ) : void;
+    add( item: CartItem, onSuccess: Function ) : void;
+    remove( item: CartItem, onSuccess: Function ) : void;
+    find( id: string, onSuccess: Function ) : void;
+    total( onSuccess: Function ) : void;
+    numberOfItems( onSuccess: Function ) : void;
+
+}
---
+++ CartItem.ts
@@ -0,0 +1,6 @@
+import Attribute from "./Attribute";
+
+export default class CartItem {
+    id: number;
+    attributes: Array<Attribute>;
+}
