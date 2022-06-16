30b3efbc9f9656db4f2a1d621b775e3decf02c04
Angel Dominguez <angeldomp49@gmail.com>
2022-06-16 12:39:06 -0500

    index.ts

---
+++ DEVNOTES.md
@@ -0,0 +1,4 @@
+
+concatenate:
+
+zalihui source1=src/Cart.ts source2=src/CartItem.ts source3=src/Attribute.ts target=index.ts
---
+++ index.ts
@@ -0,0 +1,21 @@
+export interface Cart{
+    
+    list( onSuccess: Function ) : void;
+    clean( onSuccess: Function ) : void;
+    add( item: CartItem, onSuccess: Function ) : void;
+    remove( item: CartItem, onSuccess: Function ) : void;
+    find( id: number, onSuccess: Function ) : void;
+    total( attributeName: string, onSuccess: Function ) : void;
+    numberOfItems( onSuccess: Function ) : void;
+
+}
+
+export class CartItem {
+    id: number;
+    attributes: Array<Attribute>;
+}
+
+export class Attribute{
+    name: string;
+    value: any;
+}
